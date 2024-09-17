1a509d8a513bf13cd1c3f462646c42d2f9d7cec9
Samiya <samii-1989@live.com>
2024-09-17 10:26:51 +0200

    "update URL"

--- src/components/ToggleBtn/ToggleBtn.jsx
+++ src/components/ToggleBtn/ToggleBtn.jsx
@@ -1,15 +1,20 @@
 import  { useState } from 'react';
 import styles from './ToggleBtn.module.css';
 import { PropTypes } from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 
 //The onToggle is passed from the Sidebar component, and the button toggles between user 12 and user 18.
 const ToggleBtn = ({ onToggle }) => {
   const [isOn, setIsOn] = useState(false);
-
+  const navigate = useNavigate();
+  
   const toggleSwitch = () => {
+    const newUserId = !isOn ? 18 : 12;
     setIsOn(!isOn);
-    onToggle(!isOn ? 18 : 12); // Pass the userId to the onToggle function
+    // onToggle(!isOn ? 18 : 12); // Pass the userId to the onToggle function
+    onToggle(newUserId); // Passe l'`userId` à la fonction `onToggle`
+    navigate(`/user/${newUserId}`); // Change l'URL
   };
 
   return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,17 @@
 import '../../App.css'
-import style from './Home.module.css'
-import AverageSessions from '../../components/AverageSessions/AverageSessions'
 import calorie from '../../assets/icon-calorie.png'
 import glucide from '../../assets/icon-carbohydrate.png'
 import lipide from '../../assets/icon-lipid.png'
 import proteine from '../../assets/icon-protein.png'
+import AverageSessions from '../../components/AverageSessions/AverageSessions'
 import Card from '../../components/Card/Card'
 import DailyActivity from '../../components/DailyActivity/DailyActivity'
 import Hello from '../../components/Hello/Hello'
 import Performance from '../../components/Performance/Performance'
 import Score from '../../components/Score/Score'
-// import useUserData from '../../hooks/useUserData'
-import { useUserData } from './../../hooks/useUserData';
-import { useOutletContext } from 'react-router-dom';
+import style from './Home.module.css'
+import { useOutletContext } from 'react-router-dom'
+import { useUserData } from './../../hooks/useUserData'
 
 
 //Using the useOutletContext hook to retrieve the userId from the context passed by BaseLayout.
